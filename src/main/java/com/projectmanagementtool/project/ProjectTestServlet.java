4a68e3ec6f4b3bf4e83ad594cb9338773a361cd4
Godsfavour <godsfavour@google.com>
2020-07-27 16:14:33 +0000

    lint errors

--- src/main/java/com/projectmanagementtool/project/ProjectTestServlet.java
+++ src/main/java/com/projectmanagementtool/project/ProjectTestServlet.java
@@ -5,8 +5,8 @@
 
 package com.google.sps.servlets;
 
-import com.rtb.projectmanagementtool.project.ProjectData;
 import com.rtb.projectmanagementtool.project.ProjectController;
+import com.rtb.projectmanagementtool.project.ProjectData;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,25 +15,25 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/myproject-test")
 public class ProjectTestServlet extends HttpServlet {
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Remove project: 
-        // projectController.removeProject(Long.parseLong("projectid"))
-        // Add project: 
-        // projectController.createProject("name", "description", Long.parseLong("creatorId"));
-        // Get project:
-        // projectController.getProject(Long.parseLong("projectid"))
-        // Print Project: 
-        // System.out.println("(description): \n" + projectController.getProjects().toString());
-        
-        ProjectController projectController = new ProjectController();
- 
-        System.out.println("Projects at start: \n" + projectController.getProjects().toString());
-        //projectController.createProject("Math", "workihng on derivative assignment", Long.parseLong("54672425"));
-        ProjectData mathProject = projectController.getProject(Long.parseLong("6473924464345088"));
-        mathProject.addUser(true, Long.parseLong("4673525"));
-        mathProject.addUser(false, Long.parseLong("75752354"));
-        System.out.println("Projects after change: \n" + projectController.getProjects().toString());
-        
-    }
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Remove project:
+    // projectController.removeProject(Long.parseLong("projectid"))
+    // Add project:
+    // projectController.createProject("name", "description", Long.parseLong("creatorId"));
+    // Get project:
+    // projectController.getProject(Long.parseLong("projectid"))
+    // Print Project:
+    // System.out.println("(description): \n" + projectController.getProjects().toString());
+
+    ProjectController projectController = new ProjectController();
+
+    System.out.println("Projects at start: \n" + projectController.getProjects().toString());
+    // projectController.createProject("Math", "workihng on derivative assignment",
+    // Long.parseLong("54672425"));
+    ProjectData mathProject = projectController.getProject(Long.parseLong("6473924464345088"));
+    mathProject.addUser(true, Long.parseLong("4673525"));
+    mathProject.addUser(false, Long.parseLong("75752354"));
+    System.out.println("Projects after change: \n" + projectController.getProjects().toString());
+  }
 }
