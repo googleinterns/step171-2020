4f4e60d8199cc40b2a817f194afee1c0cf77b964
Godsfavour <godsfavour@google.com>
2020-08-23 23:20:09 +0000

    more presubmit error fixes

--- src/main/webapp/scripts/project.js
+++ src/main/webapp/scripts/project.js
@@ -3,21 +3,21 @@
  */
 
 // Get some page elements
-let addUserModal = document.getElementById('add-user-to-project-modal');
-let messageModal = document.getElementById('message-modal');
-let messageModalMessage = document.getElementById('message-modal-message');
-let taskSection = document.querySelector('.project-section.tasks');
-let usersSection = document.querySelector('.project-section.users');
-let projectActions = document.querySelector('.page-header-actions');
+const addUserModal = document.getElementById('add-user-to-project-modal');
+const messageModal = document.getElementById('message-modal');
+const messageModalMessage = document.getElementById('message-modal-message');
+const taskSection = document.querySelector('.project-section.tasks');
+const usersSection = document.querySelector('.project-section.users');
+const projectActions = document.querySelector('.page-header-actions');
 // Hides the modal when clicked
 document.getElementById('modal-close').addEventListener('click',
-    function () {
+    function() {
       addUserModal.style.display = 'none';
     });
 
 // Opens the modal when clicked
 document.getElementById('add-user-button').addEventListener('click',
-    function () {
+    function() {
       document.getElementById('user-email').value = '';
       addUserModal.style.display = 'flex';
     });
@@ -25,7 +25,7 @@ document.getElementById('add-user-button').addEventListener('click',
 /**
  * Displays the message modal
  *@param {String} message the message to display
- */ 
+ */
 function showMessage(message) {
   messageModalMessage.innerHTML = message;
   messageModal.style.display = 'flex';
@@ -33,31 +33,33 @@ function showMessage(message) {
 
 // Closes the message modal
 document.getElementById('message-modal-close').addEventListener('click',
-  function () {
-    messageModal.style.display = 'none';
-  });
+    function() {
+      messageModal.style.display = 'none';
+    });
 
 /**
  * Adds a user to the project; called from addUserModal
  *@param {String} projectId id of the project
- */ 
+ */
 function addUserToProject(projectId) {
   // Get user input for userName
-  let userEmail = document.getElementById('user-email').value;
+  const userEmail = document.getElementById('user-email').value;
 
   // If userEmail is empty, show error message
-  if (!validateEmail(userEmail)) {
+  if (userEmail === '') {
     addUserModal.style.display = 'none';
     showMessage('Invalid email');
     return;
   }
 
   // Get user input for userRole
-  let userRole = document.getElementById('user-role').value;
+  const userRole = document.getElementById('user-role').value;
 
   // Generate query string & use it t call the servlet
-  let queryString = '/add-user-to-project?project=' + projectId + '&userEmail=' + userEmail + '&userRole=' + userRole;
-  fetch(queryString, {method: 'POST'}).then(response => response.json()).then((response) => {
+  const queryString = '/add-user-to-project?project=' + projectId +
+  '&userEmail=' + userEmail + '&userRole=' + userRole;
+  fetch(queryString, {method: 'POST'}).then(response =>
+  response.json()).then((response) => {
     if (response.hasOwnProperty('userId') && response.hasOwnProperty('userName')) {
       addUserToProjectPage(response.userName, userRole, response.userId);
     }
@@ -68,32 +70,23 @@ function addUserToProject(projectId) {
   });
 }
 
-/**
- * Checks if string is a valid email
- *@param {String} email email to validate
- *@return {boolean} true if valid
- */ 
-function validateEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
 // Adds the user to the page so that a page reload isn't necessary
 function addUserToProjectPage(userName, userRole, userId) {
-  let a = document.createElement('a');
-  let p = document.createElement('p');
+  const a = document.createElement('a');
+  const p = document.createElement('p');
   p.innerHTML = userRole + ': ' + userName;
   a.appendChild(p);
   a.href = '/user-profile?userID=' + userId;
   usersSection.appendChild(a);
 }
 
+// get all tabs
 const tabs = document.querySelectorAll('.project-header-tab');
 tabs.forEach(clickedTab => {
   // add onClick event for each tab
-  clickedTab.addEventListener('click', () => {
+  clickedTab.addEventListener('click', (e) => {
     // remove the active class from all tabs
-    tabs.forEach(tab => {
+    tabs.forEach((tab) => {
       tab.classList.remove('active');
     });
     let clickedClassList = clickedTab.classList;
